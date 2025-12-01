8094eb380a22b2b4c156d7c3f837dac9166bdc57
Zhuzhu-jpg <2952729214@qq.com>
2025-12-01 22:17:26 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css' // 如果有的话
-
-// 添加错误边界
-import { ErrorBoundary } from './components/ErrorBoundary'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <App />
   </React.StrictMode>,
 )
